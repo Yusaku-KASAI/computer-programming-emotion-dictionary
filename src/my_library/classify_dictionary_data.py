f0f5ffaae662421403554618592a2542ff553be5
masahiro1211 <masahiro200312@gmail.com>
2024-06-26 17:54:26 +0900

    dictionary2使えるようにしました、一応

--- src/my_library/classify_dictionary_data.py
+++ src/my_library/classify_dictionary_data.py
@@ -1,7 +1,7 @@
 def classify_dictionary_data(dictionary1, dictionary2):
     positive_words = []
     negative_words = []
-    # dictionary1のみ処理
+    # dictionary1
     for item in dictionary1:
         # 最終行の空行を除外
         if len(item) < 2:
@@ -10,4 +10,19 @@ def classify_dictionary_data(dictionary1, dictionary2):
             positive_words.append(item[0])
         if item[1] == 'n':
             negative_words.append(item[0])
+    #dictionary2
+    for item in dictionary2:
+        # 最終行の空行を除外
+        if len(item) < 2:
+            continue
+        if item[0] == "ネガ（経験）" or "ネガ（評価）":
+            items=""
+            for i in range(1,len(item)):
+                items=items+item[i]
+            negative_words.append(items)
+        elif item[0] == "ポジ（評価）" or "ポジ（経験）":
+            items=""
+            for i in range(1,len(item)):
+                items=items+item[i]
+            positive_words.append(items)
     return positive_words, negative_words
--- tests/test_predict_polarity.py
+++ tests/test_predict_polarity.py
@@ -13,22 +13,22 @@ def test_predict_positive():
     # ポジティブな入力に対する予測結果をテスト
     input_statistics = [5, 1]
     expected_output = 'positive'
-    assert predict_polarity.predict(input_statistics) == expected_output
+    assert predict_polarity.predict_polarity(input_statistics) == expected_output
 
 def test_predict_negative():
     # ネガティブな入力に対する予測結果をテスト
     input_statistics = [1, 4]
     expected_output = 'negative'
-    assert predict_polarity.predict(input_statistics) == expected_output
+    assert predict_polarity.predict_polarity(input_statistics) == expected_output
 
 def test_predict_neutral():
     # 中立的な入力に対する予測結果をテスト
     input_statistics = [3, 3]
     expected_output = 'neutral'
-    assert predict_polarity.predict(input_statistics) == expected_output
+    assert predict_polarity.predict_polarity(input_statistics) == expected_output
 
 def test_predict_empty():
     # 空の入力に対する予測結果をテスト
     input_statistics = []
     expected_output = 'neutral'
-    assert predict_polarity.predict(input_statistics) == expected_output
+    assert predict_polarity.predict_polarity(input_statistics) == expected_output
