fcef458072ba8a14235c86341d77f6e40c3df5fa
masahiro1211 <masahiro200312@gmail.com>
2024-06-27 10:41:17 +0900

    dictionary2やっと反映出来ました！

--- src/my_library/classify_dictionary_data.py
+++ src/my_library/classify_dictionary_data.py
@@ -13,7 +13,7 @@ def classify_dictionary_data(dictionary1, dictionary2):
     #dictionary2
     for item in dictionary2:
         # 最終行の空行を除外
-        if len(item) < 2:
+        if len(item) <2:
             continue
         if item[0] == "ネガ（経験）" or "ネガ（評価）":
             if not item[1] in negative_words:
@@ -22,7 +22,7 @@ def classify_dictionary_data(dictionary1, dictionary2):
             for i in range(1,len(item)):
                 items=items+item[i]
             negative_words.append(items)
-        elif item[0] == "ポジ（評価）" or "ポジ（経験）":
+        if item[0] == "ポジ（評価）"or"ポジ（経験）" :
             if not item[1] in positive_words:
                 positive_words.append(item[1])
             items=""
